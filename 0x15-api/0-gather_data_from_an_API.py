adb89c09da6bef259e098d0bb45d3e6c918f17a7
AdvantagePupil <ahmadghaleb17@gmail.com>
2024-07-29 11:15:11 -0700

    2nd time is the charm

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"""Saves to-do list information for a given employee ID to a JSON file."""
 import requests
 import sys
+import json
 
 if __name__ == "__main__":
+    user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    result = {
+        user_id: [
+            {
+                "task": task["title"],
+                "completed": task["completed"],
+                "username": user["name"]
+            }
+            for task in todos
+        ]
+    }
+
+    with open(f"{user_id}.json", "w") as f:
+        json.dump(result, f, indent=4)
+
+    print(f"To-do list for user {user['name']} (ID {user_id}) saved to {user_id}.json")
